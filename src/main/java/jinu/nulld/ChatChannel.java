57e8993abfae1ac42b041a40d15f41b7357dd7e4
NullD <zinc814.sub@gmail.com>
2021-12-28 17:07:25 +0900

    NONE(어느 곳에도 속하지 않는 구역) 추가 ADMIN(관리자) 추가
    편의를 위해 채널 이름이 아닌 채널 변수로도 입력 가능
    
    getPlayerChannel(Player player) 추가

--- src/main/java/jinu/nulld/ChatChannel.java
+++ src/main/java/jinu/nulld/ChatChannel.java
@@ -1,17 +1,17 @@
 package jinu.nulld;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public enum ChatChannel {
+    ADMIN("운영자"),
     LAW("법원"),
     PARK("공원"),
-    BANK("은행");
+    BANK("은행"),
+    NONE("없음");
 
     public final static Map<ChatChannel, List<UUID>> channelPlayers = new HashMap<>();
     public final static Map<String, ChatChannel> NAME_TO_CHANNEL = new HashMap<>();
@@ -38,6 +38,25 @@ public enum ChatChannel {
         return channelPlayers.get(getChannel(channelName));
     }
 
+    public static List<UUID> getChannelPlayers(ChatChannel channel) {
+        return channelPlayers.get(channel);
+    }
+
+    public static ChatChannel getPlayerChannel(Player player) {
+        ChatChannel cc = null;
+        for (UUID uid : getChannelPlayers("LAW")) {
+            if (Objects.equals(Bukkit.getPlayer(uid), player)) cc = LAW;
+        }
+        for (UUID uid : getChannelPlayers("PARK")) {
+            if (Objects.equals(Bukkit.getPlayer(uid), player)) cc = PARK;
+        }
+        for (UUID uid : getChannelPlayers("BANK")) {
+            if (Objects.equals(Bukkit.getPlayer(uid), player)) cc = BANK;
+        }
+
+        return cc;
+    }
+
     public static void addPlayerToChannel(Player player, String channelName) {
         List<UUID> current = getChannelPlayers(channelName);
         current.add(player.getUniqueId());
@@ -45,11 +64,25 @@ public enum ChatChannel {
         channelPlayers.put(getChannel(channelName), current);
     }
 
+    public static void addPlayerToChannel(Player player, ChatChannel channel) {
+        List<UUID> current = getChannelPlayers(channel);
+        current.add(player.getUniqueId());
+
+        channelPlayers.put(channel, current);
+    }
+
     public static void removePlayerFromChannel(Player player, String channelName) {
         List<UUID> current = getChannelPlayers(channelName);
         current.remove(player.getUniqueId());
 
         channelPlayers.put(getChannel(channelName), current);
+    }
+
+    public static void removePlayerFromChannel(Player player, ChatChannel channel) {
+        List<UUID> current = getChannelPlayers(channel);
+        current.remove(player.getUniqueId());
+
+        channelPlayers.put(channel, current);
     }
 
     public static void register() {
