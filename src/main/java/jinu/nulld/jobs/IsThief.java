b981a75553d92b52af749c4b7f9b9a64dd7f996a
KimBepo <khy010802@naver.com>
2022-01-03 09:46:12 +0900

    IsThief getThieves() 추가

--- src/main/java/jinu/nulld/jobs/IsThief.java
+++ src/main/java/jinu/nulld/jobs/IsThief.java
@@ -2,7 +2,9 @@ package jinu.nulld.jobs;
 
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class IsThief {
@@ -11,6 +13,14 @@ public class IsThief {
     public static boolean booleanThief(Player player) {
         if (findThief.get(player) != null) return findThief.get(player);
         else return false;
+    }
+
+    public static List<Player> getThieves(){
+        List<Player> players = new ArrayList<>();
+        for(Player ap : findThief.keySet()){
+            if(findThief.containsKey(ap) && findThief.get(ap)) players.add(ap);
+        }
+        return players;
     }
 
     public static void setThief(Player player) {
