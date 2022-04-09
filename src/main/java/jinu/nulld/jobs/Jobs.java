70355cd3d2b573b94115ee7d93f6c3e1110ed012
KimBepo <khy010802@naver.com>
2022-04-09 09:26:35 +0900

    Jobs usedAbility 추가

--- src/main/java/jinu/nulld/jobs/Jobs.java
+++ src/main/java/jinu/nulld/jobs/Jobs.java
@@ -16,7 +16,7 @@ public enum Jobs {
     CLERK("행정 업체 대표", "모든 투표에서 본인의 표는 2표로 취급됩니다.", 2),
     ACCOUNTING("회계 업체 대표", "토론 시간 때 1명을 지목해 대상이 해당 라운드에 금괴를 움직였는지 알 수 있습니다.", 3),
     SECURITY("보안 업체 대표", "토론 시간 때 1명을 고르면 2개의 직업이 제시됩니다. 둘 중 하나가 대상의 직업입니다.", 4),
-    INTERNET("인터넷 업체 대표", "매 밤마다 1명의 방을 훔쳐볼 수 있습니다. 소리는 들리지 않습니다.", 5),
+    INTERNET("인터넷 업체 대표", "매 밤마다 1명의 방에 1회 무조건 난입할 수 있습니다.", 5),
     CITIZEN("시민 대표", "게임 중 특정인 능력 봉인, 피고인 투표 회피 능력을 각각 한 번씩 사용할 수 있습니다.", 6),
     ARCHITECT("건축 업체 대표", "매 라운드 금고 2군데 중 진짜 금괴가 위치할 금고 1군데를 선택할 수 있습니다.", 7),
     GUARD("경비 업체 대표", "수색 시간마다 항상 첫 번째 조에 배치되며, \n해당 라운드 동안 특정인의 상자 2개를 모두 잠글 수 있는 카드키를 지급받습니다.", 8),
@@ -26,6 +26,8 @@ public enum Jobs {
     public final static Map<UUID, Jobs> jobMap = new HashMap<>();
     public final static Map<String, Jobs> JOBNAME_TO_JOBS = new HashMap<>();
 
+    private boolean abilityUsed;
+
     private final String jobName;
     private final String jobDescription;
     private final int helmetCustomData;
@@ -34,6 +36,11 @@ public enum Jobs {
         this.jobName = jobName;
         this.jobDescription = jobDescription;
         this.helmetCustomData = helmetCustomData;
+        this.abilityUsed = false;
+    }
+
+    public boolean isAbilityUsed(){
+        return this.abilityUsed;
     }
 
     public String getJobName() {
@@ -42,6 +49,11 @@ public enum Jobs {
 
     public String getJobDescription() {
         return jobDescription;
+    }
+
+
+    public void setAbilityUsed(boolean used){
+        this.abilityUsed = used;
     }
 
     public void helmet(Player p){
